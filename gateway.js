6283f33a642761b9deeed03340fc4beccffceac1
Aaron Arinder <aaronarinder@gmail.com>
2020-05-03 10:29:32 -0400

    public/private gateways

--- gateway.js
+++ gateway.js
@@ -3,17 +3,26 @@ const { ApolloGateway } = require('@apollo/gateway');
 
 // Initialize an ApolloGateway instance and pass it an array of
 // your implementing service names and URLs
-const gateway = new ApolloGateway({
-  serviceList: [
-    { name: 'viewer', url: 'http://localhost:4001' },
-    { name: 'products', url: 'http://localhost:4002' },
-    { name: 'market', url: 'http://localhost:4003' },
-    { name: 'asks', url: 'http://localhost:4004' },
-    { name: 'shipments', url: 'http://localhost:4005' },
-    { name: 'secrets', url: 'http://localhost:4006' },
-    // Define additional services here
-  ],
-});
+const gateway = process.env.NODE_ENV === 'EXTERNAL'
+    ? new ApolloGateway({
+        serviceList: [
+            { name: 'viewer', url: 'http://localhost:4001' },
+            { name: 'products', url: 'http://localhost:4002' },
+            { name: 'market', url: 'http://localhost:4003' },
+            { name: 'asks', url: 'http://localhost:4004' },
+            { name: 'shipments', url: 'http://localhost:4005' },
+        ],
+    })
+    : new ApolloGateway({
+        serviceList: [
+            { name: 'viewer', url: 'http://localhost:4001' },
+            { name: 'products', url: 'http://localhost:4002' },
+            { name: 'market', url: 'http://localhost:4003' },
+            { name: 'asks', url: 'http://localhost:4004' },
+            { name: 'shipments', url: 'http://localhost:4005' },
+            { name: 'secrets', url: 'http://localhost:4006' },
+        ],
+    })
 
 // Pass the ApolloGateway to the ApolloServer constructor
 const server = new ApolloServer({
