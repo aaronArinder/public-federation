38d111d5802bf5f071dcd5813969fa9768c2baa7
Aaron Arinder <aaronarinder@gmail.com>
2020-05-03 10:08:31 -0400

    asks resolver updated

--- asks.js
+++ asks.js
@@ -1,17 +1,18 @@
 const { ApolloServer, gql } = require('apollo-server');
 const { buildFederatedSchema } = require('@apollo/federation');
-const DataLoader = require('dataloader');
 
 const typeDefs = gql`
-    extend type Viewer @key(fields: "id") {
+    extend type User @key(fields: "id") {
+        id: ID! @external
         asks: [ Ask ]
     }
 
     extend type Product @key(fields: "id") {
-        id: String @external
+        id: ID! @external
     }
 
     type Ask {
+        id: String
         amount: Int
         product: Product
         productID: String
@@ -23,15 +24,18 @@ const typeDefs = gql`
 
 const ASKS_DATABASE = {
     'user-A': [
-        { amount: 437, active: true, productID: 'product-A', matched: false },
-        { amount: 662, active: true, productID: 'product-B', matched: false },
-        { amount: 437, active: false, productID: 'product-C', matched: true, matchedWith: 'asdf-1234' },
+        { id: '1234', amount: 437, active: true, productID: 'product-A', matched: false },
+        { id: '2345', amount: 662, active: true, productID: 'product-B', matched: false },
+        { id: '3456', amount: 437, active: false, productID: 'product-C', matched: true, matchedWith: 'asdf-1234' },
     ],
 };
 
 const resolvers = {
-    Viewer: {
-        Asks: (viewer) => viewer.id ? ASKS_DATABASE[viewer.id] : null,
+    User: {
+        asks: (user) => {
+            console.log('user', user)
+            return user.id ? ASKS_DATABASE[user.id] : null;
+        }
     },
 };
 
@@ -39,6 +43,6 @@ const server = new ApolloServer({
   schema: buildFederatedSchema([{ typeDefs, resolvers }]),
 });
 
-server.listen(4005).then(({ url }) => {
+server.listen(4004).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });
